aafd4a6a7707c86de7097a2c725ce1435a7706d0
Alex Petrash <alexp@elephantheadsoft.com>
2016-02-02 00:19:10 +0200

    implement pagination review

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product < ActiveRecord::Base
+  require 'mechanize'
+
   has_many :reviews
   has_many :user_queries
 
   def reload_reviews
-    page = Mechanize.new.get("https://www.walmart.com/reviews/product/#{self.code}")
-    reviews = page.css('p.js-customer-review-text').to_a.map(&:content).uniq
-
+    agent = Mechanize.new
+    reviews = []
+    (1..30).each do |i|
+      page = agent.get("https://www.walmart.com/reviews/product/#{self.code}?limit=20&page=#{i}&sort=relevancy")
+      reviews += page.css('p.js-customer-review-text').to_a.map(&:content).uniq
+    end
+    reviews.uniq!
     diff = reviews - self.reviews.pluck(:content)
     diff.each { |review| self.reviews.build(content: review) }
 
